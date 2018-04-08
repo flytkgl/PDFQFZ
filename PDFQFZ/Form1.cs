39456222eab466fdae09ea86ccf8c03847150a00
tkgl <flytkgl@gmail.com>
2018-04-08 23:30:14 +0800

    把引用到的DLL打包到EXE里，做成单文件，方便携带

--- PDFQFZ/Form1.cs
+++ PDFQFZ/Form1.cs
@@ -16,8 +16,20 @@ namespace PDFQFZ
 {
     public partial class Form1 : Form
     {
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)//把DLL打包到EXE需要用到
+        {
+            string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+            if (dllName.EndsWith("_resources")) return null;
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(GetType().Namespace + ".Properties.Resources", System.Reflection.Assembly.GetExecutingAssembly());
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+            return System.Reflection.Assembly.Load(bytes);
+        }
         public Form1()
         {
+            //在InitializeComponent()之前调用
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             InitializeComponent();
         }
 
