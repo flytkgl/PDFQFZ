0a346c339ae33fb1d37347e569892766669f1c47
tkgl <tkgl@163.com>
2018-04-08 12:14:37 +0800

    添加项目文件。

---
+++ PDFQFZ/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PDFQFZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_gz = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pathText = new System.Windows.Forms.TextBox();
+            this.SelectPath = new System.Windows.Forms.Button();
+            this.textBCpath = new System.Windows.Forms.TextBox();
+            this.textGZpath = new System.Windows.Forms.TextBox();
+            this.OutPath = new System.Windows.Forms.Button();
+            this.GzPath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_gz
+            // 
+            this.bt_gz.Location = new System.Drawing.Point(194, 142);
+            this.bt_gz.Name = "bt_gz";
+            this.bt_gz.Size = new System.Drawing.Size(75, 23);
+            this.bt_gz.TabIndex = 0;
+            this.bt_gz.Text = "盖章";
+            this.bt_gz.UseVisualStyleBackColor = true;
+            this.bt_gz.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pathText
+            // 
+            this.pathText.Location = new System.Drawing.Point(15, 28);
+            this.pathText.Name = "pathText";
+            this.pathText.Size = new System.Drawing.Size(339, 21);
+            this.pathText.TabIndex = 1;
+            // 
+            // SelectPath
+            // 
+            this.SelectPath.Location = new System.Drawing.Point(372, 28);
+            this.SelectPath.Name = "SelectPath";
+            this.SelectPath.Size = new System.Drawing.Size(75, 23);
+            this.SelectPath.TabIndex = 2;
+            this.SelectPath.Text = "选择";
+            this.SelectPath.UseVisualStyleBackColor = true;
+            this.SelectPath.Click += new System.EventHandler(this.SelectPath_Click);
+            // 
+            // textBCpath
+            // 
+            this.textBCpath.Location = new System.Drawing.Point(15, 72);
+            this.textBCpath.Name = "textBCpath";
+            this.textBCpath.Size = new System.Drawing.Size(339, 21);
+            this.textBCpath.TabIndex = 3;
+            // 
+            // textGZpath
+            // 
+            this.textGZpath.Location = new System.Drawing.Point(15, 115);
+            this.textGZpath.Name = "textGZpath";
+            this.textGZpath.Size = new System.Drawing.Size(339, 21);
+            this.textGZpath.TabIndex = 4;
+            // 
+            // OutPath
+            // 
+            this.OutPath.Location = new System.Drawing.Point(373, 72);
+            this.OutPath.Name = "OutPath";
+            this.OutPath.Size = new System.Drawing.Size(75, 23);
+            this.OutPath.TabIndex = 5;
+            this.OutPath.Text = "选择";
+            this.OutPath.UseVisualStyleBackColor = true;
+            this.OutPath.Click += new System.EventHandler(this.OutPath_Click);
+            // 
+            // GzPath
+            // 
+            this.GzPath.Location = new System.Drawing.Point(373, 115);
+            this.GzPath.Name = "GzPath";
+            this.GzPath.Size = new System.Drawing.Size(75, 23);
+            this.GzPath.TabIndex = 6;
+            this.GzPath.Text = "选择";
+            this.GzPath.UseVisualStyleBackColor = true;
+            this.GzPath.Click += new System.EventHandler(this.GzPath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "请选择需要盖章的PDF文件所在目录";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "请选择PDF盖章后想保存在的目录";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "请选择印章文件";
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 171);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log.Size = new System.Drawing.Size(435, 108);
+            this.log.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 291);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GzPath);
+            this.Controls.Add(this.OutPath);
+            this.Controls.Add(this.textGZpath);
+            this.Controls.Add(this.textBCpath);
+            this.Controls.Add(this.SelectPath);
+            this.Controls.Add(this.pathText);
+            this.Controls.Add(this.bt_gz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PDF加盖骑缝章";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_gz;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox pathText;
+        private System.Windows.Forms.Button SelectPath;
+        private System.Windows.Forms.TextBox textBCpath;
+        private System.Windows.Forms.TextBox textGZpath;
+        private System.Windows.Forms.Button OutPath;
+        private System.Windows.Forms.Button GzPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox log;
+    }
+}
+
---
+++ PDFQFZ/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.Drawing.Imaging;
+
+namespace PDFQFZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(pathText.Text);
+            var fileInfos = dir.GetFiles();
+            string sourcepath= pathText.Text;//需盖章目录
+            string outputpath= textBCpath.Text;//保存目录
+            string watermark = textGZpath.Text;  // 水印图片
+
+            if (!Directory.Exists(outputpath))//输出目录不存在则新建
+            {
+                Directory.CreateDirectory(outputpath);
+            }
+
+            log.Text = "";//清空日志
+
+                try
+            {
+                foreach (var fileInfo in fileInfos)
+                {
+                    string source = sourcepath + "\\" + fileInfo.Name.ToString();
+                    string output = outputpath + "\\" + fileInfo.Name.ToString();
+                    bool isSurrcess = PDFWatermark(source, output, watermark);
+                    if (isSurrcess)
+                    {
+                        log.Text = log.Text + "成功！“" + fileInfo.Name.ToString() + "” 盖章完成！\r\n";
+                    }else
+                    {
+                        log.Text = log.Text + "失败！“" + fileInfo.Name.ToString() + "” 盖章失败！\r\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        public static Bitmap[] subImages(String imgPath, int n)
+        {
+            Bitmap[] nImage = new Bitmap[n];
+            Bitmap img = new Bitmap(imgPath);
+            int h = img.Height;
+            int w = img.Width;
+            int sw = w / n;
+            for (int i = 0; i < n; i++)
+            {
+                Bitmap newbitmap = new Bitmap(sw, h);
+                Graphics g = Graphics.FromImage(newbitmap);
+                g.DrawImage(img, new System.Drawing.Rectangle(0, 0, sw, h), new System.Drawing.Rectangle(sw * i, 0, sw, h), GraphicsUnit.Pixel);
+                g.Dispose();
+                nImage[i] = newbitmap;
+                //newbitmap.Save("D://" + i +".png", ImageFormat.Png);//查看图片是否正常
+            }
+            return nImage;
+        }
+
+        public bool PDFWatermark(string inputfilepath, string outputfilepath, string ModelPicName)
+
+        {
+            //throw new NotImplementedException();
+            PdfReader pdfReader = null;
+            PdfStamper pdfStamper = null;
+            try
+            {
+                pdfReader = new PdfReader(inputfilepath);//选择需要印章的pdf
+
+                int numberOfPages = pdfReader.NumberOfPages;//pdf页数
+
+                iTextSharp.text.Rectangle psize = pdfReader.GetPageSize(1);
+
+                float width = psize.Width;
+
+                float height = psize.Height;
+
+                Bitmap[] nImage = subImages(ModelPicName, numberOfPages);
+
+                pdfStamper = new PdfStamper(pdfReader, new FileStream(outputfilepath, FileMode.Create));//加完印章后的pdf
+
+                PdfContentByte waterMarkContent;
+                
+                //每一页加水印,也可以设置某一页加水印
+                for (int i = 0; i < numberOfPages; i++)
+                {
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(nImage[i], ImageFormat.Png);
+
+                //image.GrayFill = 20;//透明度，灰色填充
+                                    //image.Rotation//旋转
+                                    //image.RotationDegrees//旋转角度
+                                    //水印的位置
+
+                    image.SetAbsolutePosition(width-image.Width, (height - image.Height) /2);
+                    waterMarkContent = pdfStamper.GetUnderContent(i+1);
+
+                    waterMarkContent.AddImage(image);
+                    
+
+                }
+                //strMsg = "success";
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Message.Trim();
+                return false;
+            }
+            finally
+            {
+
+                if (pdfStamper != null)
+                    pdfStamper.Close();
+
+                if (pdfReader != null)
+                    pdfReader.Close();
+            }
+        }
+
+        private void SelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                pathText.Text = fbd.SelectedPath;
+                textBCpath.Text = fbd.SelectedPath + "\\QFZ";
+            }
+        }
+
+        private void OutPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBCpath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void GzPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "图片文件|*.jpg;*.png";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textGZpath.Text = file.FileName;
+            }
+        }
+    }
+}
